73a0ba48e0f5eb53968b5f667808b2a51d7fa896
katiRG <cnangini@gmail.com>
2015-10-07 00:23:01 +0200

    Legend defined in leafletBase

--- web/js/dc.leaflet.js
+++ web/js/dc.leaflet.js
@@ -49,10 +49,54 @@ dc_leaflet.leafletBase = function(_chart) {
         return L.map(child_div.node(),_mapOptions);
     };
 
+    //======================================================================
+    //Legend code adapted from http://leafletjs.com/examples/choropleth.html
+
+    var _legend = L.control({position: 'bottomleft'});
+    var _colorDomain;
+    var minValue, maxValue, palette, colorLength, delta, grades;
+
+    _legend.onAdd = function (map) {          
+        this._div = L.DomUtil.create('div', 'info legend');
+        this.update();      
+        return this._div;
+    };
+
+    _legend.update = function () {
+        if (_chart.colorDomain()) {//check because undefined for marker charts        
+            minValue = _chart.colorDomain()[0];
+            maxValue = _chart.colorDomain()[1];
+            palette = _chart.colors().range();
+            colorLength = _chart.colors().range().length;
+            delta = (maxValue - minValue)/colorLength;             
+
+            //define grades for legend colours
+            //based on equation in dc.js colorCalculator (before verion based on colorMixin)
+            grades = [];
+            grades[0] = minValue;
+            for (var i= 1; i < colorLength; i++) {
+                grades[i] = Math.round((0.5 + (i - 1)) * delta + minValue);
+            }          
+              
+            var div = L.DomUtil.create('div', 'info legend'),          
+                labels = [];            
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            this._div.innerHTML = ""; //reset so that legend is not plotted multiple times
+            for (var i = 0; i < grades.length; i++) {
+                this._div.innerHTML +=
+                    '<i style="background:' + palette[i] + '"></i> ' +
+                      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            }
+        }
+    };        
+    //======================================================================
+
+
     var _tiles=function(map) {
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(map);        
     };
 
     _chart.createLeaflet = function(_) {
@@ -63,7 +107,7 @@ dc_leaflet.leafletBase = function(_chart) {
         return _chart;
     };
 
-    _chart._doRender = function() {
+    _chart._doRender = function() {        
         _map = _createLeaflet(_chart.root());
         for(var ev in _cachedHandlers)
             _map.on(ev, _cachedHandlers[ev]);
@@ -73,11 +117,25 @@ dc_leaflet.leafletBase = function(_chart) {
         }
 
         _chart.tiles()(_map);
+        
+        _chart.legend().addTo(_map);
 
         _chart._postRender();
 
         return _chart._doRedraw();
     };
+
+    _chart.legend = function(_) {
+        if (!arguments.length) return _legend;
+        _legend = _;
+        return _chart;
+    }
+
+    _chart.colorDomain = function(_) {
+        if (!arguments.length) return _colorDomain;
+        _colorDomain = _;
+        return _chart;
+    }
 
     _chart._postRender = function() {
         //abstract
@@ -450,7 +508,7 @@ dc_leaflet.choroplethChart = function(parent, chartGroup) {
         return options;
     };
 
-    var _popup = function(d,feature) {
+    var _popup = function(d,feature) {        
         return _chart.title()(d);
     };
 
@@ -469,6 +527,8 @@ dc_leaflet.choroplethChart = function(parent, chartGroup) {
             _dataMap[_chart.keyAccessor()(d)] = {'d':d,'i':i};
         });
         _geojsonLayer.addData(_chart.geojson());
+
+        _chart.legend().update();
     };
 
     _chart.geojson = function(_) {
@@ -532,7 +592,7 @@ dc_leaflet.choroplethChart = function(parent, chartGroup) {
         if (v && v.d) {
             layer.key=v.d.key;
             if (_chart.renderPopup())
-                layer.bindPopup(_chart.popup()(v.d,feature));
+                layer.bindPopup(_chart.popup()(v.d,feature));            
             if (_chart.brushOn())
                 layer.on("click",selectFilter);
         }
