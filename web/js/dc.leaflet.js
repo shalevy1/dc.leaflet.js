76ac49ebf9eb2735ff9aac5f12f6950adac83f01
Alan Kavanagh <alan@unravel.ie>
2015-12-15 15:40:38 +0000

    add updated web resources

--- web/js/dc.leaflet.js
+++ web/js/dc.leaflet.js
@@ -1,5 +1,5 @@
 /*!
- *  dc.leaflet 0.2.2
+ *  dc.leaflet 0.2.3
  *  http://dc-js.github.io/dc.leaflet.js/
  *  Copyright 2014-2015 Boyan Yurukov and the dc.leaflet Developers
  *  https://github.com/dc-js/dc.leaflet.js/blob/master/AUTHORS
@@ -20,7 +20,7 @@
 'use strict';
 
 var dc_leaflet = {
-    version: '0.2.2'
+    version: '0.2.3'
 };
 
 dc_leaflet.leafletBase = function(_chart) {
@@ -50,8 +50,8 @@ dc_leaflet.leafletBase = function(_chart) {
     };
 
     var _tiles=function(map) {
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
     };
 
@@ -72,9 +72,8 @@ dc_leaflet.leafletBase = function(_chart) {
             if (_defaultCenter && _defaultZoom) {
                 _map.setView(_chart.toLocArray(_defaultCenter), _defaultZoom);
             }
-        
+
             _chart.tiles()(_map);
-       
             _chart._postRender();
         }
         else
